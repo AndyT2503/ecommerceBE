aac5b0159a507b43119656c957246206edcac729
Phương Nguyễn <nvpchaytran@gmail.com>
2021-10-11 21:47:59 +0700

    Add Order Dto

---
+++ Ecommerce.Application/Orders/Dto/OrderDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Application.Orders.Dto
+{
+    public class OrderDto
+    {
+        public Guid CategoryId { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
